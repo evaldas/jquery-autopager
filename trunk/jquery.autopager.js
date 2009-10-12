5dc869ca416a2c21298f9b058e8427391f556191
lagos.on <lagos.on@1f5dce32-6376-11de-9867-89bd1bae666e>
2009-10-12 18:07:00 +0000

    release 1.0.0

--- trunk/jquery.autopager.js
+++ trunk/jquery.autopager.js
@@ -1,67 +1,167 @@
 /*
- * AutoPager plugin for jQuery
+ * Automatic paging plugin for jQuery
  *
- * Copyright (c) 2009 lagos
+ * Copyright (c) lagos
  * Dual licensed under the MIT and GPL licenses.
  *
  * @auther  lagos
- * @version 0.1.1
+ * @version 1.0.0
  */
 (function($) {
-    $.autopager = function(options) {
-        var nextPageUrl;
-        var requestedPages = [location.href];
-        var loading = false;
-        if ($.browser.msie) {
-             var scrollTopElement = $('html');
-             var heightElement = $(document);
-        } else {
-             var scrollTopElement = $(document);
-             var heightElement = $('html');
+    var window = this, options = {},
+        content, currentUrl, nextUrl,
+        active = false;
+
+    $.autopager = function(_options) {
+        if (typeof _options === 'string' && $.isFunction(this.autopager[_options])) {
+            var args = Array.prototype.slice.call(arguments, 1);
+            return this.autopager[_options].apply(this.autopager, args);
         }
 
-        options = $.extend({
-            insertBefore: $(options.pageElement).next(),
-            pagingPoint: 350,
+        _options = $.extend({
+            autoLoad: true,
+            page: 1,
+            content: '.content',
+            link: 'a[rel=next]',
+            insertBefore: null, 
+            appendTo: null, 
+            start: function() {},
             load: function() {},
-            success: function() {}
-        }, options);
+            disabled: false
+        }, _options);
+        this.autopager.option(_options);
 
-        setNextPageUrl();
-
-        $(window).scroll(function(){
-            if (!loading && nextPageUrl && $.inArray(nextPageUrl, requestedPages) < 0 && scrollHeightRemain() < options.pagingPoint) {
-                loading = true;
-                requestedPages.push(nextPageUrl);
-                $.get(nextPageUrl, function(data){
-                    var nextPage = $('<div/>').html(data);
-                    setNextPageUrl(nextPage);
-                    insertPageElement(nextPage);
-                    loading = false;
-                });
+        content = $(_options.content).filter(':last');
+        if (content.length) {
+            if (!_options.insertBefore && !_options.appendTo) {
+                var insertBefore = content.next();
+                if (insertBefore.length) {
+                    set('insertBefore', insertBefore);
+                } else {
+                    set('appendTo', content.parent());
+                }
             }
-        });
+        }
+
+        setUrl();
 
         return this;
-
-        function setNextPageUrl(context) {
-            var args = [options.nextLink];
-            if (context) args.push(context);
-            nextPageUrl = $.apply(null, args).attr('href');
-        }
-
-        function insertPageElement(context) {
-            var pageElement = $(options.pageElement, context); 
-            if (pageElement) {
-                options.load.call(pageElement, requestedPages.length);
-                pageElement.insertBefore(options.insertBefore);
-                options.success.call(pageElement, requestedPages.length);
-            }
-        }
-
-        function scrollHeightRemain() {
-            return heightElement.height() - scrollTopElement.scrollTop() - $(window).height();
-        }
     };
-})(jQuery);
 
+    $.extend($.autopager, {
+        option: function(key, value) {
+            var _options = key;
+
+            if (typeof key === "string") {
+                if (value === undefined) {
+                    return options[key];
+                }
+                _options = {};
+                _options[key] = value;
+            }
+
+            $.each(_options, function(key, value) {
+                set(key, value);
+            });
+        },
+
+        enable: function() {
+            set('disabled', false);
+        },
+
+        disable: function() {
+            set('disabled', true);
+        },
+
+        destroy: function() {
+            this.autoLoad(false);
+            options = {};
+            content = currentUrl = nextUrl = undefined;
+        },
+
+        autoLoad: function(value) {
+            if (value === undefined) {
+                return options.autoLoad;
+            }
+            set('autoLoad', value);
+        },
+
+        load: function() {
+            if (active || !nextUrl || options.disabled) {
+                return;
+            }
+
+            active = true;
+            options.start(currentHash(), nextHash());
+            $.get(nextUrl, insertContent);
+        }
+
+    });
+
+	function set(key, value) {
+		switch (key) {
+			case 'autoLoad':
+                if (value && !options.autoLoad) {
+                    $(window).scroll(loadOnScroll);
+                } else if (!value && options.autoLoad) {
+                    $(window).unbind('scroll', loadOnScroll);
+                }
+                break;
+            case 'insertBefore':
+                if (value) {
+                    options.appendTo = null;
+                }
+                break
+            case 'appendTo':
+                if (value) {
+                    options.insertBefore = null;
+                }
+                break
+		}
+		options[key] = value;
+	}
+
+    function setUrl(context) {
+        currentUrl = nextUrl || window.location.href;
+        nextUrl = $(options.link, context).attr('href');
+    }
+
+    function loadOnScroll() {
+        if (content.offset().top + content.height() < $(document).scrollTop() + $(window).height()) {
+            $.autopager.load();
+        }
+    }
+
+    function insertContent(res) {
+        var _options = options,
+            nextPage = $('<div/>').append(res.replace(/<script(.|\s)*?\/script>/g, "")),
+            nextContent = nextPage.find(_options.content); 
+
+        set('page', _options.page + 1);
+        setUrl(nextPage);
+        if (nextContent.length) {
+            if (_options.insertBefore) {
+                nextContent.insertBefore(_options.insertBefore);
+            } else {
+                nextContent.appendTo(_options.appendTo);
+            }
+            _options.load.call(nextContent.get(), currentHash(), nextHash());
+            content = nextContent.filter(':last');
+        }
+        active = false;
+    }
+
+    function currentHash() {
+        return {
+            page: options.page,
+            url: currentUrl
+        };
+    }
+
+    function nextHash() {
+        return {
+            page: options.page + 1,
+            url: nextUrl
+        };
+    }
+})(jQuery);
